b0048572dbeea35f2ac7d92bbde1136ccf9e2ce1
Zack <zackakil94@gmail.com>
2021-05-04 14:26:04 +0100

    boxes being drawn

--- components/object_tracking.js
+++ components/object_tracking.js
@@ -10,7 +10,7 @@ document.getElementsByTagName('head')[0].appendChild(style);
 
 // define component
 Vue.component('object-tracking-viz', {
-    props: ['json_data'],
+    props: ['json_data', 'video_height', 'video_width'],
     data: function () {
         return {
         }
@@ -37,7 +37,7 @@ Vue.component('object-tracking-viz', {
                 return []
 
             this.object_tracks.forEach(element => {
-                indexed_tracks.push(new Object_Track(element))
+                indexed_tracks.push(new Object_Track(element, this.video_height, this.video_width))
             })
 
             return indexed_tracks
@@ -48,12 +48,55 @@ Vue.component('object-tracking-viz', {
         <h3>Object tracking</h3>
         <p v-for="item in object_tracks">This is my item : {{item}}</p>
     </div>
-    `
+    `,
+    mounted: function () {
+        var canvas = document.getElementById("my_canvas")
+        var ctx = canvas.getContext("2d")
+        ctx.strokeStyle = "#FF0000"
+
+        ctx.beginPath()
+        ctx.rect(20, 20, 150, 100)
+        ctx.stroke()
+
+        ctx.beginPath();
+        ctx.rect(20, 20, 150, 100);
+        ctx.fillStyle = "red";
+        ctx.fill();
+
+        
+
+        var time_update_interval = setInterval(function () {
+            const object_tracks =  document.querySelector('#object_tracks').__vue__.indexed_object_tracks
+            console.log('intevrkjabks', object_tracks)
+            draw_bounding_boxes(object_tracks, ctx)
+        }, 1000 / 30)
+    }
 })
 
-// var time_update_interval = setInterval(function () {
-//     updateTimerDisplay();
-// }, 1000)
+
+function draw_bounding_boxes(object_tracks, ctx) {
+    console.log(video.currentTime)
+    ctx.clearRect(0, 0, 500, 500)
+
+    const current_time = video.currentTime
+
+    object_tracks.forEach(tracked_object => {
+
+        if (tracked_object.has_frames_for_time(current_time)){
+            console.log('drawing box for ', tracked_object)
+            draw_bounding_box(tracked_object.current_bounding_box(current_time), ctx)
+        }
+
+    })
+}
+
+function draw_bounding_box(box, ctx) {
+    console.log('drawing', box)
+    ctx.strokeStyle = "#000000"
+    ctx.beginPath()
+    ctx.rect(box.x, box.y, box.width, box.height)
+    ctx.stroke()
+}
 
 
 function nullable_time_offset_to_seconds(time_offset) {
@@ -64,7 +107,7 @@ function nullable_time_offset_to_seconds(time_offset) {
 
 
 class Object_Track {
-    constructor(json_data) {
+    constructor(json_data, video_height, video_width) {
         this.name = json_data.entity.description
         this.start_time = nullable_time_offset_to_seconds(json_data.segment.start_time_offset)
         this.end_time = nullable_time_offset_to_seconds(json_data.segment.end_time_offset)
@@ -74,10 +117,10 @@ class Object_Track {
         json_data.frames.forEach(frame => {
             const new_frame = {
                 'box': {
-                    'left': frame.normalized_bounding_box.left || 0,
-                    'right': frame.normalized_bounding_box.right || 0,
-                    'top': frame.normalized_bounding_box.top || 0,
-                    'bottom': frame.normalized_bounding_box.bottom || 0
+                    'x': (frame.normalized_bounding_box.left || 0) * video_width,
+                    'y': (frame.normalized_bounding_box.top || 0) * video_height,
+                    'width': ((frame.normalized_bounding_box.right || 0) - (frame.normalized_bounding_box.left || 0)) * video_width,
+                    'height': ((frame.normalized_bounding_box.bottom || 0) - (frame.normalized_bounding_box.top || 0)) * video_height
                 },
                 'time_offset': nullable_time_offset_to_seconds(frame.time_offset)
             }
@@ -85,13 +128,16 @@ class Object_Track {
         })
     }
 
+    has_frames_for_time(seconds) {
+        return ((this.start_time <= seconds) && (this.end_time >= seconds))
+    }
 
-    current_bounding_box(seconds){
+    current_bounding_box(seconds) {
 
         for (let index = 0; index < this.frames.length; index++) {
-            if (this.frames[index].time_offset > seconds){
+            if (this.frames[index].time_offset > seconds) {
                 if (index > 0)
-                    return this.frames[index - 1]
+                    return this.frames[index - 1].box
                 else
                     return null
             }

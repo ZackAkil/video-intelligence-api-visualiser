03d4cca2ab11ed24859fe3c45358a5eaef85c845
Zack <zackakil94@gmail.com>
2021-05-15 22:50:46 +0100

    tweak transcipt

--- components/speech_transcription.js
+++ components/speech_transcription.js
@@ -12,15 +12,17 @@ style.innerHTML = `
 }
 
 .speech{
-    padding:5px;
+    padding:15px;
+    margin:0;
 }
 
 .current_word{
-    border-bottom: solid #4285F4 2px;
+    border-bottom: solid #DB4437 5px;
 }
 
 .current_speech{
-    border-left: solid #0F9D58 2px;
+
+    box-shadow: 0 2px 2px 0 rgb(0 0 0 / 14%), 0 3px 1px -2px rgb(0 0 0 / 20%), 0 1px 5px 0 rgb(0 0 0 / 12%);
 }
 `;
 document.getElementsByTagName('head')[0].appendChild(style);

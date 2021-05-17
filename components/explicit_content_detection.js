c7a467289a0ff14a24905a3c54c75206a57e9e6e
Zack <zackakil94@gmail.com>
2021-05-17 12:39:33 +0100

    updated explicit visual

--- components/explicit_content_detection.js
+++ components/explicit_content_detection.js
@@ -2,7 +2,10 @@
 var style = document.createElement('style');
 style.innerHTML = `
 
-
+.current-likelihood{
+    width:150px;
+    display: inline-block;
+}
 `;
 document.getElementsByTagName('head')[0].appendChild(style);
 
@@ -13,7 +16,8 @@ Vue.component('explicit-content-detection-viz', {
     data: function () {
         return {
             interval_timer: null,
-            current_time: 0
+            current_time: 0,
+            likelihoods: ['VERY_UNLIKELY', 'UNLIKELY', 'POSSIBLE', 'LIKELY', 'VERY_LIKELY']
         }
     },
     computed: {
@@ -45,43 +49,81 @@ Vue.component('explicit-content-detection-viz', {
 
             return indexed_shots
         },
+
+        likelihood_segments: function () {
+            `
+            Create a clean list of explicit content detetcion segments
+            `
+            const segments = {
+                'VERY_LIKELY': { 'segments': [], 'count': 0 },
+                'LIKELY': { 'segments': [], 'count': 0 },
+                'POSSIBLE': { 'segments': [], 'count': 0 },
+                'UNLIKELY': { 'segments': [], 'count': 0 },
+                'VERY_UNLIKELY': { 'segments': [], 'count': 0 },
+            }
+
+            this.indexed_explicit_frames_annotations.forEach(shot => {
+
+                segments[shot.explicit_liklyhood].segments.push(shot.time_offset)
+
+            })
+
+            return segments
+        },
+        current_likelihood :function (){
+
+            for (let index = 0; index < this.indexed_explicit_frames_annotations.length; index++) {
+                const element = this.indexed_explicit_frames_annotations[index]
+                if (element.time_offset > this.current_time)
+                    return this.indexed_explicit_frames_annotations[ Math.max(index - 1, 0 )].explicit_liklyhood
+            }
+
+            return ''
+        }
     },
     methods: {
-        // segment_style: function (segment) {
-        //     return {
-        //         left: ((segment.start_time / this.video_info.length) * 100).toString() + '%',
-        //         width: (((segment.end_time - segment.start_time) / this.video_info.length) * 100).toString() + '%'
-        //     }
-        // },
-        shot_clicked: function (shot_data) {
-            this.$emit('shot-clicked', { seconds: shot_data.time_offset })
+        segment_style: function (segment) {
+            return {
+                left: ((segment / this.video_info.length) * 100).toString() + '%',
+                width: '5px'
+            }
         },
-        // label_on_screen: function (label) {
-        //     return label.has_segment_for_time(this.current_time)
-        // }
+        shot_clicked: function (shot_data) {
+            this.$emit('shot-clicked', { seconds: shot_data })
+        }
     },
     template: `
     <div calss="shot_detection-container">
 
     <div class="data-warning" v-if="explicit_frames_annotations.length == 0"> No explicit content detection data in JSON</div>
 
-    <div v-for="shot in indexed_explicit_frames_annotations" 
-    v-on:click="shot_clicked(shot)"> {{shot.time_offset}} -> {{shot.explicit_liklyhood}}</div>
+    <div>Current explicit content likelihood : <span class="current-likelihood">{{current_likelihood}}</span> </div>
+    <br>
+
+    <div class="segment-container" v-for="segments, key in likelihood_segments" v-bind:key="key + 'z'">
+                <div class="label">{{key}}</div>
+                <div class="segment-timeline">
+                    <div class="segment" v-for="segment in segments.segments" 
+                                        v-bind:style="segment_style(segment)" 
+                                        v-on:click="shot_clicked(segment)"
+                    ></div>
+                </div>
+            </div>
 
     </div>
     `,
     mounted: function () {
         console.log('mounted component')
 
-        // const component = this
+        const component = this
 
-        // this.interval_timer = setInterval(function () {
-        //     component.current_time = video.currentTime
-        // }, 1000 / 10)
+        this.interval_timer = setInterval(function () {
+            component.current_time = video.currentTime
+        }, 1000 / 10)
     },
     beforeDestroy: function () {
         console.log('destroying component')
-        // clearInterval(this.interval_timer)
+        clearInterval(this.interval_timer)
     }
 })
 
